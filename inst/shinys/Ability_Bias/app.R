29fd25378052620f495b200d94f20f44ee049a60
dxy99999 <36264545+dxy99999@users.noreply.github.com>
2020-09-13 16:30:31 +0800

    add new Apps for R shiny

---
+++ inst/shinys/Ability_Bias/app.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(MASS)
+??mvnorm
+x<-mvrnorm(n=100,mu = c(0,0), Sigma = matrix(data = c(1,0,0,1),2,2))
+plot(x[,1],x[,2])
+z<-runif(n=100)
+
+cor(x[,1],z)
+cor(x[,2],z)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
